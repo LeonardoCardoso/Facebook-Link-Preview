d8d2373c705c54efcb476233fa448d67fbbdd693
Leonardo <leo@leocardz.com>
2015-04-20 02:10:16 +0100

    Logging error_reporting switched off in retrieve.php. It was causing problems on json interpretation

--- php/classes/SetUp.php
+++ php/classes/SetUp.php
@@ -13,13 +13,14 @@ class SetUp {
 
     /** Set charset utf 8 */
     static function headers(){
+        error_reporting(0);
         header("Content-Type: text/html; charset=utf-8", true);
     }
 
     /** Allow url fopen to crawl the code */
     static function init(){
         SetUp::headers();
-        error_reporting(false);
+        error_reporting(0);
         if (!ini_get('allow_url_fopen'))
             ini_set('allow_url_fopen', 1);
     }
